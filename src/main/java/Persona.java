1df7b1c4b98039387189565e17100a3e45052a32
Jhiguitam <jhiguitam@unal.edu.co>
2022-04-03 11:37:39 -0500

    a

---
+++ src/main/java/Persona.java
@@ -0,0 +1,55 @@
+package classroom;
+
+public class Persona {
+
+     long cedula;
+    String nombre;
+    static int totalPersonas;
+    
+    static {
+        totalPersonas = 0;
+        //cedula = 3;
+    }
+
+    Persona(long cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
+    }
+    
+    Persona(String nombre, long cedula) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
+    }
+
+    Persona(long cedula) {
+        this.cedula = cedula;
+        this.nombre = "";
+        totalPersonas++;
+    }
+
+    Persona(String nombre) {
+        this.nombre = "";
+        this.cedula=1;
+        totalPersonas++;
+    }
+    
+    public Persona() {
+		this.cedula=0;
+		this.nombre="";
+		totalPersonas++;
+	}
+
+	long getCedula() {
+        return cedula;
+    }
+
+    String getNombre() {
+        return nombre;
+    }
+
+    void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
